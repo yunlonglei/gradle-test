e4e92d9617758d8fdc46ed4e2138a449680803ab
yunlonglei <yunlonglei0@gmail.com>
2021-12-30 11:29:10 +0800

    :clapper: testdemo and publish

---
+++ web/src/test/java/com/lei/web/TestDome.java
@@ -0,0 +1,17 @@
+package com.lei.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author leiyunlong
+ * @version 1.0
+ * @since 2021/12/30 10:24 AM
+ */
+@SpringBootTest
+public class TestDome {
+    @Test
+    public void testMethod(){
+        System.out.println(1/1);
+    }
+}
